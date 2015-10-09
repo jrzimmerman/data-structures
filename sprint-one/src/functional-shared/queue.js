d94cd3ab06afcd001dffe6bb961be1b327389c85
Justin Zimmerman <zimmy130@gmail.com>
2015-10-08 20:03:49 -0400

    complete functional-shared stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,25 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.count = 0;
+  obj.storage = {};
+
+  obj.enqueue = queueMethods.enqueue;
+  obj.size = queueMethods.size;
+
+  return obj;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
+
+queueMethods.size = function() {
+  return this.count;
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var obj = {};
+  obj.count = 0;
+  obj.storage = {};
+
+  // calls to methods
+  obj.push = stackMethods.push;
+  obj.size = stackMethods.size;
+  obj.pop = stackMethods.pop;
+  return obj;
+
 };
 
 var stackMethods = {};
+// push
+stackMethods.push = function(value){
+  this.storage[this.count] = value;
+  this.count++;
+};
+// pop
+stackMethods.pop = function(){
+  if( this.count > 0 ) {
+    this.count--;  
+  }
+  return this.storage[this.count];
+}
 
-
+// size
+stackMethods.size = function(){
+  return this.count;
+};
